0bdc1d0d09827ff4d733b5c3bda0c71ad237b81c
loit152 <loit152152@gmail.com>
2026-02-07 00:13:34 +0900

    ui

--- s.js
+++ s.js
@@ -5,9 +5,13 @@ let heavy=document.getElementById('heavy');
 	heavy.style.display="block";
 let vol=document.getElementById('vol');
 let amo=document.getElementById('amo');
-let sub=document.getElementsByClassName('sub')[0];
+let form=document.getElementById("sim");
 /*クリック時の動作*/
-sub.addEventListener('click',check);
+form.addEventListener("keydown",e =>{
+	if(e.key !== "Enter") return;
+	check(e);
+});
+
 let step=1
 function create(e){
 			let newDiv=document.createElement('div');
@@ -18,34 +22,33 @@ function create(e){
 function check(e){
 	e.preventDefault();
 	if(step==1){
-		let heavyValue=document.querySelector('#sim [name="q1"]').value;
+		let heavyValue=document.querySelector('[name="q1"]').value;
 		if(heavyValue>0){
 			step=2;
 			vol.style.display="block";
 			heavy.style.display="none";
 			create(heavyValue);
-			document.querySelecaaator("[name='q2']").focus();
+			document.querySelector("[name='q2']").focus();
 		}
 	}
 	else if(step==2){
-		let volValue=document.querySelector('#sim [name="q2"]').value;
+		let volValue=document.querySelector('[name="q2"]').value;
 		if(volValue>0){
 			step=3;
 			amo.style.display="block";
 			vol.style.display="none";
 			create(volValue);
+			document.querySelector('[name="q3"]').focus();
 		}
 	}
 	else if(step==3){
-		let amoValue=document.querySelector('#sim [name="q3"]').value;
+		let amoValue=document.querySelector('[name="q3"]').value;
 		if(amoValue>0){
 			step=4;
 			amo.style.display="none";
-			sub.style.display="none";
 			create(amoValue);
 		}
 	}
-	c(e);
 }
 		
 
