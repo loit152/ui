644a2bd50bbc6e9b75a7a36ccc0d1849ad71d986
loit152 <loit152152@gmail.com>
2026-07-26 15:24:08 +0900

    うい

--- fractal/script.js
+++ fractal/script.js
@@ -1,1 +1,18 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+const width = canvas.width;
+const height = canvas.height;
+
+const image = ctx.createImageData(width, height);
+const pixels = image.data;
+
+for(let y=0; y<height; y++){
+
+    for(let x=0; x<width; x++){
+
+    }
+
+}
+const real = x / width * 3.5 - 2.5;
+const imag = y / height * 2 - 1;
