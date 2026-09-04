941548b1f778ea00e035a8a29863305fc5fa4cd6
loit152 <loit152152@gmail.com>
2026-09-04 11:37:05 +0900

    ui

--- fractal/reconstruction/script.js
+++ fractal/reconstruction/script.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("webgl2");
-const hight = document.HTMLCanvasElement.height;
+const height = document.HTMLCanvasElement.height;
 const width = document.HTMLCanvasElement.width;
 const console = document.getElementById("console");
 
-console.innerHTML= +hight+width;
+console.innerHTML += `height: ${height}, width: ${width}<br>`;
