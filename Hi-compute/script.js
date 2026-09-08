3451610d51b5acf39cab74b049322e76f43af1dd
loit152 <loit152152@gmail.com>
2026-09-08 13:28:30 +0900

    ui

--- Hi-compute/script.js
+++ Hi-compute/script.js
@@ -3,7 +3,7 @@ const gl = canvas.getContext("webgl2");
 
 const xInput = document.querySelector("#coordinatex");
 const yInput = document.querySelector("#coordinatey");
-const result = document.getElementById("#result");
+const result = document.getElementById("result");
 
 function calculate() {
     const x = Number(xInput.value);
