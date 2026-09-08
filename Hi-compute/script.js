6eb9f8b940c11f9fe52ad34d6dcbb623869d0770
loit152 <loit152152@gmail.com>
2026-09-08 12:52:41 +0900

    ui

--- Hi-compute/script.js
+++ Hi-compute/script.js
@@ -2,11 +2,12 @@ const canvas = document.getElementById("canvas");;
 const gl = camvas.getContext("webgl2");
 const x = 0;
 const y = 0;
+const multiplication = 0;
 const input = document.querySelector("input");
 
 input.addEventListener(input , () => {
     x = document.querySelector("coordinaterx");
     y = document.querySelector("coordinatery");
-
+    multiplication = x * y;
     }
 )
