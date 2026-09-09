042acbb3da9e98c515cb3a566dc7196fb1e36fef
loit152 <loit152152@gmail.com>
2026-09-09 10:24:12 +0900

    完成

--- Hi-compute/script.js
+++ Hi-compute/script.js
@@ -8,10 +8,10 @@ const result = document.getElementById("result");
 function calculate() {
     const x = Number(xInput.value);
     const y = Number(yInput.value);
-
+    
     let r = x * y;
     let s = 0;
-
+let n =r;
     while (r !== 0) {
         const i = r % 10;
         r = Math.floor(r / 10);
@@ -20,7 +20,7 @@ function calculate() {
         console.log(i);
     }
 
-    result.innerHTML = `r = ${r}, s = ${s}`;
+    result.innerHTML = `n = ${n},r = ${r}, s = ${s}`;
 }
 
 xInput.addEventListener("input", calculate);
