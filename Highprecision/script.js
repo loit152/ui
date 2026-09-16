c0128e48c1a5171aba44df262739caa6ed08bcc0
loit152 <loit152152@gmail.com>
2026-09-16 17:26:56 +0900

    ui

--- Highprecision/script.js
+++ Highprecision/script.js
@@ -4,34 +4,37 @@ const gl = canvas.getContext("webgl2");
 const xInput = document.querySelector("#coordinatex");
 const yInput = document.querySelector("#coordinatey");
 const result = document.getElementById("result");
-let a=0;
+
+let a = 0;
+
 function calculate() {
     const x = Number(xInput.value);
     const y = Number(yInput.value);
-    
+
     let r = x * y;
+    const n = r;
     let s = 0;
-let n =r;
+
     while (r !== 0) {
         const i = r % 10;
         r = Math.floor(r / 10);
         s++;
-
     }
+
     a = n;
-    result.innerHTML = `n = ${n},r = ${r}, s = ${s}`;
+
+    result.innerHTML = `n = ${n}, r = ${r}, s = ${s}`;
 }
 
 xInput.addEventListener("input", calculate);
 yInput.addEventListener("input", calculate);
 
-document.addEventListener("keydown",function(event){
-        if (event.key ==="enter"){
-            event.preventDfault();
-            yInput.value = n;
-        }
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        yInput.value = a;
+        calculate();
     }
-)
+});
 
-/*first*/
-calculate()
+calculate();
