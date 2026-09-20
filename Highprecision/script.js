1d57a99600119b7c8f9f9c58b5e1cf8fe5d9129f
loit152 <loit152152@gmail.com>
2026-09-20 22:41:35 +0900

    delete js concerned canvas and mroe

--- Highprecision/script.js
+++ Highprecision/script.js
@@ -7,24 +7,6 @@ const result = document.getElementById("result");
 
 let a = 0;
 
-function calculate() {
-    const x = Number(xInput.value);
-    const y = Number(yInput.value);
-
-    let r = x * y;
-    const n = r;
-    let s = 0;
-
-    while (r !== 0) {
-        const i = r % 10;
-        r = Math.floor(r / 10);
-        s++;
-    }
-
-    a = n;
-
-    result.innerHTML = `n = ${n}, r = ${r}, s = ${s}`;
-}
 
 xInput.addEventListener("input", calculate);
 yInput.addEventListener("input", calculate);
