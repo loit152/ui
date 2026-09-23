ef9913d847f4493c44fe18a5f9a96d6879079a94
loit152 <loit152152@gmail.com>
2026-09-23 19:19:43 +0900

    restert

--- fractal/reconstruction/script.js
+++ fractal/reconstruction/script.js
@@ -1,12 +1,12 @@
 /*自動更新後で消す*/
 setInterval(() => {
   location.reload();
-}, 5000);
+}, 1000);
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("webgl2");
 const height = window.innerHeight;
 const width = window.innerWidth;
-const con = document.getElementById("console");
+
 
 con.innerHTML += `height: ${height}, width: ${width}<br>`;
