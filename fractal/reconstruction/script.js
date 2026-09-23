aa2adf68e56d5e29ed8488e497ec95e187b1de84
loit152 <loit152152@gmail.com>
2026-09-23 19:37:43 +0900

    統合　html js

--- fractal/reconstruction/script.js
+++ fractal/reconstruction/script.js
@@ -2,11 +2,13 @@
 setInterval(() => {
   location.reload();
 }, 1000);
-
+/*初期情報*/
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("webgl2");
 const height = window.innerHeight;
 const width = window.innerWidth;
-
-
+/*info */
+const zoom = document.getElementById("zoom");
+const centerX = document.getElementById("zcenterX");
+centerX.innerHTML = 
 con.innerHTML += `height: ${height}, width: ${width}<br>`;
